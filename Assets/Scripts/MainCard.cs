dc2c56b5d04069474e180b6d9c8bdb48c6ecbd1e
Crazyayane07 <crazyayane07@gmail.com>
2018-03-17 08:04:21 +0100

    Initial commit

---
+++ Assets/Scripts/MainCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour {
+
+    [SerializeField]
+    private SceneController controller;
+    [SerializeField]
+    private GameObject Back;
+    public int id { get; set; }
+
+    public void OnMouseDown()
+    {
+        if (Back.activeSelf && controller.canWork())
+        {
+            setFaceCard();
+            controller.showed(this);
+        }
+    }
+
+    public void setFaceAndId(int id, Sprite Face)
+    {
+        this.id = id;
+        GetComponent<SpriteRenderer>().sprite = Face;
+    }
+
+    public void setBackCard()
+    {
+        Back.SetActive(true);
+    }
+
+    public void setFaceCard()
+    {
+        Back.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject controler;
+    [SerializeField]
+    private string function;
+
+    public void OnMouseUp()
+    {
+            controler.SendMessage(function);
+    }
+}
